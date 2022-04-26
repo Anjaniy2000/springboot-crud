798b49ea93068aa9f574cd2fe174753723d5b714
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-04-26 16:07:29 +0530

    third commit

---
+++ src/main/java/com/anjaniy/springbootcrud/repository/StudentRepo.java
@@ -0,0 +1,7 @@
+package com.anjaniy.springbootcrud.repository;
+
+import com.anjaniy.springbootcrud.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepo extends JpaRepository<Student, Integer> {
+}
