7c7bb8d071c14c0bf832869bdd86059bcf0bb4d0
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-05-06 00:01:56 +0530

    tenth commit

--- src/main/java/com/anjaniy/springbootcrud/SpringbootcrudApplication.java
+++ src/main/java/com/anjaniy/springbootcrud/SpringbootcrudApplication.java
@@ -1,9 +1,12 @@
 package com.anjaniy.springbootcrud;
 
+import com.anjaniy.springbootcrud.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class SpringbootcrudApplication {
 
 	public static void main(String[] args) {
